928d9a6978938b8d2247f7876459090c3c59dbd6
haiman <83174104+haiman233@users.noreply.github.com>
2022-04-23 17:27:38 +0800

    汉化

--- src/main/java/com/github/relativobr/supremeexpansion/gear/basic/BasicShovel.java
+++ src/main/java/com/github/relativobr/supremeexpansion/gear/basic/BasicShovel.java
@@ -10,7 +10,7 @@ public final class BasicShovel {
 
   public static final SlimefunItemStack ADAMANTIUM_SHOVEL = new SlimefunItemStack(
       "TOOLS_ADAMANTIUM_SHOVEL",
-      Material.DIAMOND_SHOVEL, "&f&l精金铲", "&f&l由精金制成");
+      Material.DIAMOND_SHOVEL, "&f&l精金锹", "&f&l由精金制成");
   public static final ItemStack[] RECIPE_ADAMANTIUM_SHOVEL = {
       null, Components.ADAMANTIUM_PLATE, null,
       null, new ItemStack(Material.STICK), null,
@@ -18,7 +18,7 @@ public final class BasicShovel {
   };
 
   public static final SlimefunItemStack AURUM_SHOVEL = new SlimefunItemStack("TOOLS_AURUM_SHOVEL",
-      Material.DIAMOND_SHOVEL, "&f&l金合金铲", "&f&l由金合金制成");
+      Material.DIAMOND_SHOVEL, "&f&l金合金锹", "&f&l由金合金制成");
   public static final ItemStack[] RECIPE_AURUM_SHOVEL = {
       null, Components.AURUM_PLATE, null,
       null, new ItemStack(Material.STICK), null,
@@ -27,7 +27,7 @@ public final class BasicShovel {
 
   public static final SlimefunItemStack TITANIUM_SHOVEL = new SlimefunItemStack(
       "TOOLS_TITANIUM_SHOVEL",
-      Material.DIAMOND_SHOVEL, "&f&l钛合金铲", "&f&l由钛合金制成");
+      Material.DIAMOND_SHOVEL, "&f&l钛合金锹", "&f&l由钛合金制成");
   public static final ItemStack[] RECIPE_TITANIUM_SHOVEL = {
       null, Components.TITANIUM_PLATE, null,
       null, new ItemStack(Material.STICK), null,
