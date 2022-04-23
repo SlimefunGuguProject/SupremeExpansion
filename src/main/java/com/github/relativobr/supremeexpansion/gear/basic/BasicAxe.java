02ea3ee1d5c22dcfbd94f578215c1fddfdfbdc5f
haiman <83174104+haiman233@users.noreply.github.com>
2022-04-23 12:49:58 +0800

    汉化

--- src/main/java/com/github/relativobr/supremeexpansion/gear/basic/BasicAxe.java
+++ src/main/java/com/github/relativobr/supremeexpansion/gear/basic/BasicAxe.java
@@ -10,7 +10,7 @@ public final class BasicAxe {
 
   public static final SlimefunItemStack ADAMANTIUM_AXE = new SlimefunItemStack(
       "TOOLS_ADAMANTIUM_AXE",
-      Material.DIAMOND_AXE, "&f&lAdamantium Axe", "&f&lA Axe of Adamantium");
+      Material.DIAMOND_AXE, "&f&l精金斧", "&f&l由精金制成");
   public static final ItemStack[] RECIPE_ADAMANTIUM_AXE = {
       Components.ADAMANTIUM_PLATE, Components.ADAMANTIUM_PLATE, null,
       Components.ADAMANTIUM_PLATE, new ItemStack(Material.STICK), null,
@@ -18,7 +18,7 @@ public final class BasicAxe {
   };
 
   public static final SlimefunItemStack AURUM_AXE = new SlimefunItemStack("TOOLS_AURUM_AXE",
-      Material.DIAMOND_AXE, "&f&lAurum Axe", "&f&lA Axe of Aurum");
+      Material.DIAMOND_AXE, "&f&l金合金斧", "&f&l由金合金制成");
   public static final ItemStack[] RECIPE_AURUM_AXE = {
       Components.AURUM_PLATE, Components.AURUM_PLATE, null,
       Components.AURUM_PLATE, new ItemStack(Material.STICK), null,
@@ -26,7 +26,7 @@ public final class BasicAxe {
   };
 
   public static final SlimefunItemStack TITANIUM_AXE = new SlimefunItemStack("TOOLS_TITANIUM_AXE",
-      Material.DIAMOND_AXE, "&f&lTitanium Axe", "&f&lA Axe of Titanium");
+      Material.DIAMOND_AXE, "&f&l钛合金斧", "&f&l由钛合金制成");
   public static final ItemStack[] RECIPE_TITANIUM_AXE = {
       Components.TITANIUM_PLATE, Components.TITANIUM_PLATE, null,
       Components.TITANIUM_PLATE, new ItemStack(Material.STICK), null,
