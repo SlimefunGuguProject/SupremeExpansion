fb297c02cd42b1f9b98f17e7afd7b143e1de8816
haiman <83174104+haiman233@users.noreply.github.com>
2022-04-23 11:54:58 +0800

    汉化

--- src/main/java/com/github/relativobr/supremeexpansion/generators/Capacitor.java
+++ src/main/java/com/github/relativobr/supremeexpansion/generators/Capacitor.java
@@ -20,7 +20,7 @@ public class Capacitor {
   public static final int SUPREME_CAPACITY = 1600000000; // infinity
 
   public static final SlimefunItemStack AURUM_CAPACITOR = new SlimefunItemStack("AURUM_CAPACITOR",
-      "db2b1c052e84a4ad245a4d1ca5b10bd7f5249af0ca373f99174ce7990e597836", "&9Aurum Capacitor", "",
+      "db2b1c052e84a4ad245a4d1ca5b10bd7f5249af0ca373f99174ce7990e597836", "&9金电容", "",
       LoreBuilder.machine(MachineTier.ADVANCED, MachineType.CAPACITOR),
       Energy.energyCapacity(AURUM_CAPACITY));
   public static final ItemStack[] MACHINE_TITANIUM_CAPACITOR = new ItemStack[]{
@@ -35,7 +35,7 @@ public class Capacitor {
   };
   public static final SlimefunItemStack TITANIUM_CAPACITOR = new SlimefunItemStack(
       "TITANIUM_CAPACITOR",
-      "b30ad864b265ab178a03e27fc249a1e32941e007486febd0059e4e3b76c11958", "&9Titanium Capacitor",
+      "b30ad864b265ab178a03e27fc249a1e32941e007486febd0059e4e3b76c11958", "&9钛电容",
       "",
       LoreBuilder.machine(MachineTier.ADVANCED, MachineType.CAPACITOR),
       Energy.energyCapacity(TITANIUM_CAPACITY));
@@ -46,7 +46,7 @@ public class Capacitor {
   };
   public static final SlimefunItemStack ADAMANTIUM_CAPACITOR = new SlimefunItemStack(
       "ADAMANTIUM_CAPACITOR",
-      "9ac52419b99025828c89fa825945e6948e45bb5a22e4425a59e9096e4c1ac38", "&9Adamantium Capacitor",
+      "9ac52419b99025828c89fa825945e6948e45bb5a22e4425a59e9096e4c1ac38", "&9精金电容",
       "",
       LoreBuilder.machine(MachineTier.ADVANCED, MachineType.CAPACITOR),
       Energy.energyCapacity(ADAMANTIUM_CAPACITY));
@@ -57,7 +57,7 @@ public class Capacitor {
   };
   public static final SlimefunItemStack THORNIUM_CAPACITOR = new SlimefunItemStack(
       "THORNIUM_CAPACITOR",
-      "9aab01a5d9512e3175bf3af2edbfd59419dc5662605c42c91730e711254efcc4", "&9Thornium Capacitor",
+      "9aab01a5d9512e3175bf3af2edbfd59419dc5662605c42c91730e711254efcc4", "&9钍电容",
       "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.CAPACITOR),
       Energy.energyCapacity(THORNIUM_CAPACITY));
@@ -68,9 +68,8 @@ public class Capacitor {
   };
   public static final SlimefunItemStack SUPREME_CAPACITOR = new SlimefunItemStack(
       "SUPREME_CAPACITOR",
-      "77400ea19dbd84f75c39ad6823ac4ef786f39f48fc6f84602366ac29b837422", "&9Supreme Capacitor",
-      "&c&oDo not use more than ",
-      "&c&o1 per energy network",
+      "77400ea19dbd84f75c39ad6823ac4ef786f39f48fc6f84602366ac29b837422", "&9至尊电容",
+      "&c&o切勿在多个能源网中使用",
       "",
       LoreBuilder.machine(MachineTier.END_GAME, MachineType.CAPACITOR),
       Energy.energyCapacity(null));
