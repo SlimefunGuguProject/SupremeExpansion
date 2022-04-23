5168131ca247a5eeaa992875d555402b07b599e6
haiman <83174104+haiman233@users.noreply.github.com>
2022-04-23 10:57:39 +0800

    汉化

--- src/main/java/com/github/relativobr/supremeexpansion/magical/Cetrus.java
+++ src/main/java/com/github/relativobr/supremeexpansion/magical/Cetrus.java
@@ -10,8 +10,8 @@ public class Cetrus {
 
 
   public static final SlimefunItemStack CETRUS_LUX = new SlimefunItemStack("CETRUS_LUX",
-      "92cb6e51c461e7359526bea5e06209cddde7c6469a819f3405cf0a038c159502", "&aCetrus Lux", "",
-      "&7A super lux scepter");
+      "92cb6e51c461e7359526bea5e06209cddde7c6469a819f3405cf0a038c159502", "&a金行", "",
+      "&7五行之一");
   public static final ItemStack[] RECIPE_CETRUS_LUX = {
       new ItemStack(Material.FLINT, 64), new ItemStack(Material.GLOWSTONE, 64),
       new ItemStack(Material.TORCH, 64),
@@ -22,8 +22,8 @@ public class Cetrus {
   };
 
   public static final SlimefunItemStack CETRUS_VENTUS = new SlimefunItemStack("CETRUS_VENTUS",
-      "b28f1c0c5092e12d33770df45c5845a9610886039b34abe93a16c5e942dfc8e4", "&aCetrus Ventus", "",
-      "&7A super ventus scepter");
+      "b28f1c0c5092e12d33770df45c5845a9610886039b34abe93a16c5e942dfc8e4", "&a木行", "",
+      "&7五行之一");
   public static final ItemStack[] RECIPE_CETRUS_VENTUS = {
       new ItemStack(Material.FEATHER, 64), new ItemStack(Material.LEATHER, 64),
       new ItemStack(Material.CHORUS_FRUIT, 64),
@@ -34,8 +34,8 @@ public class Cetrus {
   };
 
   public static final SlimefunItemStack CETRUS_LUMIUM = new SlimefunItemStack("CETRUS_LUMIUM",
-      "4b1cce22de19ed6727abc5e6c2d57864c871a44c956bbe2eb3960269b686b8b3", "&aCetrus Lumium", "",
-      "&7A super lumium scepter");
+      "4b1cce22de19ed6727abc5e6c2d57864c871a44c956bbe2eb3960269b686b8b3", "&a土行", "",
+      "&7五行之一");
   public static final ItemStack[] RECIPE_CETRUS_LUMIUM = {
       new ItemStack(Material.DIRT, 64), new SlimefunItemStack(SupremeCore.CORE_OF_BLOCK, 1),
       new ItemStack(Material.GRASS_BLOCK, 64),
@@ -46,8 +46,8 @@ public class Cetrus {
   };
 
   public static final SlimefunItemStack CETRUS_AQUA = new SlimefunItemStack("CETRUS_AQUA",
-      "36161daa3589ec9c8187459ac36fd4dd2646c040678d3bfacb72a2210c6c801c", "&aCetrus Aqua", "",
-      "&7A super aqua scepter");
+      "36161daa3589ec9c8187459ac36fd4dd2646c040678d3bfacb72a2210c6c801c", "&a水行", "",
+      "&7五行之一");
   public static final ItemStack[] RECIPE_CETRUS_AQUA = {
       new ItemStack(Material.ICE, 64), new ItemStack(Material.LILY_PAD, 64),
       new ItemStack(Material.SALMON, 64),
@@ -58,8 +58,8 @@ public class Cetrus {
   };
 
   public static final SlimefunItemStack CETRUS_IGNIS = new SlimefunItemStack("CETRUS_IGNIS",
-      "e672959028f274b379d430f068f0f15a4f793eac12afb94ae0b4e50cf895df0f", "&aCetrus Ignis", "",
-      "&7A super ignis scepter");
+      "e672959028f274b379d430f068f0f15a4f793eac12afb94ae0b4e50cf895df0f", "&a火行", "",
+      "&7五行之一");
   public static final ItemStack[] RECIPE_CETRUS_IGNIS = {
       new ItemStack(Material.NETHERRACK, 64), new ItemStack(Material.BLAZE_ROD, 64),
       new ItemStack(Material.SOUL_SAND, 64),
